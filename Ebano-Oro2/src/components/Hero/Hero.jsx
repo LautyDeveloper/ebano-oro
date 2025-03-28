4753b3738c0e91ec5541d56edec45ece56bde1b1
Lautaro Aquino <laufabio24@gmail.com>
2025-03-28 00:31:39 -0300

    Finishing Hero styles

--- Ebano-Oro2/src/components/Hero/Hero.jsx
+++ Ebano-Oro2/src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ export const Hero = () => {
                 <div className="hero-text">
                     <p className="hero-phrase">La excelencia en cada bocado, el lujo en cada detalle.</p>
                     <h1 className="hero-title">Ebano Oro</h1>
-                    <p className="hero-subtitle">"Un rincón de sofisticación y sabores únicos. Reservá tu mesa y viví la experiencia."</p>
+                    <p className="hero-subtitle">Un rincón de sofisticación y sabores únicos. Reservá tu mesa y viví la experiencia.</p>
                 </div>
                 <div className="hero-button-container">
                     <button className="hero-button">RESERVA YA</button>
