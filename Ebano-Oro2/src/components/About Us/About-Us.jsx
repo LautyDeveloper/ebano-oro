c59ecf479e29d00392903f604896ffe9d8906db7
Lauty Dev <lautydev24@gmail.com>
2025-03-30 14:08:53 -0300

    Finish about us card

--- Ebano-Oro2/src/components/About Us/About-Us.jsx
+++ Ebano-Oro2/src/components/About Us/About-Us.jsx
@@ -12,12 +12,12 @@ export const AboutUs = () => {
                     <div className="about-us-card">
                         <div className="about-us-card-text">
                             <p className="about-us-card-phrase">momentos especiales</p>
-                            <h2 className="about-us-card-title">Sofisticado & Exclusivo</h2>
+                            <h2 className="about-us-card-title">Sofisticado <br /> & Exclusivo</h2>
                             <p className="about-us-card-parragraph">En Noir & Or, tradición y modernidad se fusionan en cada plato.</p>
                         </div>
                         <button className="about-us-card-button">Reserva Ya</button>
                     </div>
-                </div>
+                </div>  
 
                 <div className="about-us-image-container" style={{gridRow: 3/4  }}>
                     <img src="" alt="" className="about-us-image" />
