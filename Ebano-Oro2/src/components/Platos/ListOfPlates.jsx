76c70e505526a5c813bdcee7c8d026346431f210
Lauty Dev <lautydev24@gmail.com>
2025-04-02 16:42:13 -0300

    Poder teñir Secciones

--- Ebano-Oro2/src/components/Platos/ListOfPlates.jsx
+++ Ebano-Oro2/src/components/Platos/ListOfPlates.jsx
@@ -1,20 +1,29 @@
+import './ListOfPlates.css'
 export const ListOfPlates = ({phrase, title, image, platos, invested}) => {
     return(
-        <div className={`platos-container ${invested ? "invested" : ""}`}>
-        <img src={image} alt={`Imagen de ${title}`} />
-        <div className="plato-info">
-            <p className="plato-phrase">{phrase}</p>
-            <h2 className="plato-title">{title}</h2>
-            <div className="platos-list">
-                {platos.map(plato => {
-                    <div className="platos-item">
-                        <h3 className="platos-item-title">{plato.title}</h3>
-                        <p className="platos-item-desc">{plato.desc}</p>
-                        <span className="platos-item-price">{plato.price}</span>
-                    </div>
-                })}
+        <div className = {`platos ${invested ? "platos-black" : "platos-blue"}`} >
+            <div className={`platos-container ${invested ? "invested" : ""}`}>
+            <img src={image} alt={`Imagen de ${title}`} />
+            <div className="plato-info">
+                <div className="plato-info-header">
+                    <p className="plato-info-phrase">{phrase}</p>
+                    <h2 className="plato-info-title">{title}</h2>    
+                </div>
+                <div className="platos-list">
+                    {platos.map(plato => {
+                        return (
+                        <div className="platos-item">
+                            <div className="platos-item-header">
+                                <h3 className="platos-item-header-title">{plato.title}</h3>
+                                <p className="platos-item-header-desc">{plato.desc}</p>
+                            </div>
+                            <span className="platos-item-price">{plato.price}</span>
+                        </div>   
+                        )
+                    })}
+                </div>
+            </div>
             </div>
         </div>
-    </div>
     )
 }
