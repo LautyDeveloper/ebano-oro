55243237a99bc562b76323548f65b87138d04b6a
Lautaro Aquino <laufabio24@gmail.com>
2025-04-28 14:44:54 -0300

    Añadimos mapa interactivo

--- Ebano-Oro2/src/pages/contact/Contact.jsx
+++ Ebano-Oro2/src/pages/contact/Contact.jsx
@@ -10,7 +10,17 @@ export const Contact = () => {
       />
 
       <div className="contacto-content">
-        <div className="contacto-mapa-container"></div>
+        <div className="contacto-mapa-container">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1034.312273300251!2d-58.36263142986707!3d-34.61081577797033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a335282ffd829d%3A0x3a1e4ae6db6becd6!2sOsaka!5e0!3m2!1sen!2sar!4v1745861986064!5m2!1sen!2sar"
+            width="100%"
+            height="100%"
+            style={{ border: 0, borderRadius: "10px" }}
+            allowFullScreen="true"
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
         <div className="contacto-targets-container">
           <Target image={null} title={"Instagram"} parragraph={"@noiretoro"} />
           <Target
